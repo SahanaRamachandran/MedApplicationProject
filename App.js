d032134a431c01f0d14812fce845a6733fbd0177
SahanaRamachandran <124963679+SahanaRamachandran@users.noreply.github.com>
2024-08-16 07:18:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// // App.js
+// import React from "react";
+// import { BrowserRouter, Route } from 'react-router-dom';
+// import About from "./pages/About";
+// import Home from "./pages/Home";
+// function App() {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//       <Route path="/" Component={<Home/>}/>
+//       <Route path="/about" Component={<About/>}/>
+//       <BrowserRouter/>
+//     </div>
+//   );
+// }
+// export default App;
